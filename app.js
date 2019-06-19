b9a78736021d47a996a69f6f503d9055375425eb
Benjamin Clark <benisin@gmail.com>
2019-06-19 13:57:54 -0700

    got storelsData working. now need to get initlsData working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var dataStorageKey = 'Product Data';
 var imageArray = [
     "img/bag.jpg",
     "img/banana.jpg",
@@ -23,26 +24,47 @@ var imageArray = [
     "img/wine-glass.jpg"
 ];
 var totalClicks = 0;
-var maxClicks = 25;
+var maxClicks = 10;
 var previousSetOfImages = [];
 var chartContext = document.getElementById('chart').getContext('2d');
 
 
 
-function Product(filepath) {
+function Product(filepath, displayCount, clickCount) {
     this.filepath = filepath;
-    this.displayed = 0;
-    this.clicks = 0;
+    this.name = filepath.split('/')[1].split('.')[0];
+    this.displayCount = displayCount;
+    this.clickCount = clickCount;
+
     Product.all.push(this);
 }
 Product.all = [];
 
+function initlsData() {
+
+    // is there Productdata in local storage
+    var lsObject = localStorage.getItem(dataStorageKey);
+    if (lsObject) {
+        // parse it
+        // create all products again
+
+    } else {
+        // if not create new products
+        imageArray.forEach(element => {
+            new Product(element, 0, 0);
+        });
+    }
+
+}
+
+function storelsData() {
+    var stringifyData = JSON.stringify(Product.all);
+    localStorage.setItem(dataStorageKey, stringifyData);
+}
 
 function setup() {
-    // create all products
-    imageArray.forEach(element => {
-        new Product(element);
-    });
+    initlsData();
+
     //setup listeners
     setupListener()
 }
@@ -76,7 +98,7 @@ function loadRandomImages() {
             // If it hsan't been previously shown and its not on screen ...
             if (!previousSetOfImages.includes(ranProductIndex) && !currentSetOfImages.includes(ranProductIndex)) {
 
-                Product.all[ranProductIndex].displayed++;
+                Product.all[ranProductIndex].displayCount++;
 
                 // Render it
                 image.src = Product.all[ranProductIndex].filepath;
@@ -98,10 +120,14 @@ function getRandProductIndex() {
 
 
 function handleClick(event) {
+
+    // go through all products 
     for (var i = 0; i < Product.all.length; i++) {
+        // when you find the prodcut increment totalClicks, clickCount
         if (Product.all[i].filepath === event.target.alt) {
-            Product.all[i].clicks++;
+            Product.all[i].clickCount++;
             totalClicks++;
+            // remove click listener and load chart
             if (totalClicks >= maxClicks) {
                 removeListener();
                 triggerResults();
@@ -109,6 +135,9 @@ function handleClick(event) {
             break;
         }
     }
+    // store all product data to local storage
+    storelsData();
+    // load new images
     loadRandomImages();
 }
 
@@ -120,22 +149,26 @@ function triggerResults() {
 }
 
 function renderChart() {
+
     console.log('renderChart called');
 
     var labels = [];
     var voteData = [];
     var colors = [];
 
+    // set product percentages
     for (var i = 0; i < Product.all.length; i++) {
-        Product.all[i].percentage = Math.round(Product.all[i].clicks / Product.all[i].displayed * 100);
+        Product.all[i].percentage = Math.round(Product.all[i].clickCount / Product.all[i].displayCount * 100);
     }
 
+    // sort list greatest to smallest
     Product.all.sort(function (a, b) {
         return b.percentage - a.percentage;
     });
 
+    // populate labels, votData, and colors arays for the chart.
     for (var j = 0; j < Product.all.length; j++) {
-        labels.push(Product.all[j].filepath);
+        labels.push(Product.all[j].name);
         voteData.push(Product.all[j].percentage);
         var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
         colors.push(randomColor);
